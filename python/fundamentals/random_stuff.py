bbf072dd60014f9f4134e45d308522406778dcfc
Alex Patrucco <faytheffect@gmail.com>
2019-03-14 00:04:43 -0400

    Added some random stuff, and some facial recognition code!

---
+++ python/fundamentals/random_stuff.py
@@ -0,0 +1,28 @@
+# Fibonacci
+
+# def fib(num):
+#     if(num == 0):
+#         return 0
+#     elif(num == 1):
+#         return 1
+#     else:
+#         return fib(num-1) + fib(num-2)
+
+# print(fib(6))
+
+# x = [ [5,2,3], [10,8,9] ] 
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'}
+# ]
+# sports_directory = {
+#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# }
+# z = [ {'x': 10, 'y': 20} ]
+
+# x[1][0] = 15
+# students[0]['last_name'] = 'Bryant'
+# sports_directory['soccer'][0] = 'Andres'
+# z[0]['y'] = 30
+
